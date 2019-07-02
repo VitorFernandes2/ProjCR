9bca2792390422f07e4c8562ceb3191555e5e31f
ProgVitor <vhferprog@gmail.com>
2019-07-03 00:03:07 +0100

    correção feita na irisdataset

--- main.m
+++ main.m
@@ -1,5 +1,6 @@
 function main()
 
+clc;
 clear all;
 close all;
 
@@ -206,10 +207,26 @@ disp(tr)
 
 % SIMULAR
 %simula a rede neuronal treinada com os novos dados de entrada
-out = sim(net, SimulationMatrix);
+out = sim(net, InputMatrix);
 
 %VISUALIZAR DESEMPENHO
 
 plotconfusion(TargetMatrix, out) % Matriz de confusao
 
 plotperf(tr)         % Grafico com o desempenho da rede nos 3 conjuntos  
+
+%Calcula e mostra a percentagem de classificacoes corretas no total dos exemplos
+r=0;
+for i=1:size(out,2)               % Para cada classificacao  
+  [a b] = max(out(:,i));          %b guarda a linha onde encontrou valor mais alto da saida obtida
+  [c d] = max(TargetMatrix(:,i));  %d guarda a linha onde encontrou valor mais alto da saida desejada
+  if b == d                       % se estao na mesma linha, a classificacao foi correta (incrementa 1)
+      r = r+1;
+  end
+end
+
+accuracy = r/size(out,2)*100;
+fprintf('Precisao total %.2f%%\n', accuracy)
+
+end
+
