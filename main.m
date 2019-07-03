dfcf64d69fad840c267b3f8ba438efcf163e095e
Joap_coelhp <jpcoelho99@gmail.com>
2019-07-03 22:24:26 +0100

    treino e simulacao pra forma 1 funcional (alpha)

--- main.m
+++ main.m
@@ -70,6 +70,19 @@ outputmatrix= zeros(4,4);
     end
     
  end
+ 
+ 
+ %SimulationMatrix = zeros(NumInputs, 4);
+
+for i=1:4
+   
+    ImageName = sprintf('ImagensLeitura\\%d.png', i - 1);
+    Img = imread(ImageName);
+    Img = imbinarize(Img);
+    SimulationMatrix(1:40000,i)=Img(:); % input <- mudar o nome
+
+end
+ 
 %{
 net = feedforwardnet(NumberNeurons);
 net.divideFcn = '';
@@ -91,15 +104,15 @@ net.layers{1}.transferFcn = 'tansig';
 
 net.trainFcn = 'trainscg';
 
-%net.divideFcn = ''; topico A
+net.divideFcn = ''; %topico A
 
 % topico B \/
-
+%{
 net.divideFcn = 'dividerand';
 net.divideParam.trainRatio = 0.7;
 net.divideParam.valRatio = 0.15;
 net.divideParam.testRatio = 0.15;
-
+%}
 
 %Treinar
 %treina com os dados de entrada e com a matriz de objectivos
@@ -114,7 +127,7 @@ out = sim(net, SimulationMatrix);
 
 %VISUALIZAR DESEMPENHO
 
-plotconfusion(TargetMatrix, out) % Matriz de confusao
+plotconfusion(outputmatrix, out) % Matriz de confusao
 
 plotperf(tr)         % Grafico com o desempenho da rede nos 3 conjuntos  
 
@@ -122,7 +135,7 @@ plotperf(tr)         % Grafico com o desempenho da rede nos 3 conjuntos
 r=0;
 for i=1:size(out,2)               % Para cada classificacao  
   [a b] = max(out(:,i));          %b guarda a linha onde encontrou valor mais alto da saida obtida
-  [c d] = max(TargetMatrix(:,i));  %d guarda a linha onde encontrou valor mais alto da saida desejada
+  [c d] = max(outputmatrix(:,i));  %d guarda a linha onde encontrou valor mais alto da saida desejada
   if b == d                       % se estao na mesma linha, a classificacao foi correta (incrementa 1)
       r = r+1;
   end
